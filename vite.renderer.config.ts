6c0054e1e5860ed95a7e8baeb27140aafb9b70d2
Anton Reshetov <reshetov.art@gmail.com>
2024-01-29 09:17:28 +0300

    chore: add autoimport components

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,12 +1,20 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
 
 const pathSrc = path.resolve(__dirname, './src/renderer')
 
 // https://vitejs.dev/config
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      dirs: [`${pathSrc}/components`],
+      extensions: ['vue'],
+      dts: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': pathSrc,
