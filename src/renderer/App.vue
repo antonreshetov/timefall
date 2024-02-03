0670f7c4bd8cae3cf5d5fa05baee0acee321d278
Anton Reshetov <reshetov.art@gmail.com>
2024-02-03 07:01:31 +0300

    feat: add titlebar drag area

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -8,8 +8,17 @@ useDark()
   <div
     class="h-screen grid grid-cols-[var(--sidebar-width)_var(--tasks-width)_1fr]"
   >
+    <div
+      data-titlebar
+      class="fixed top-0 w-full h-[20px] -select-none z-50"
+    />
     <Sidebar />
-    <Tasks />
     <RouterView />
   </div>
 </template>
+
+<style>
+[data-titlebar] {
+  -webkit-app-region: drag;
+}
+</style>
