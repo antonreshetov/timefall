df959ba316e474d6977c51a69dd63ea19a567c78
Anton Reshetov <reshetov.art@gmail.com>
2024-02-03 07:06:52 +0300

    feat(ui): add topbar

---
+++ src/renderer/components/ui/topbar/Topbar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { cn } from '@/utils'
+
+interface Props {
+  class?: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div
+    data-ui-topbar
+    :class="
+      cn(
+        'h-[var(--topbar-height)] sticky top-0 z-10 dark:bg-neutral-900 bg-white w-full flex items-center',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </div>
+</template>
